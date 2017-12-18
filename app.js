b5ef852f3814bf0d43fb27e79bc5068f75b3e578
Ellis Mwekunda <Scareb15@gmail.com>
2017-12-18 09:34:23 -0700

    setup for express server

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+
+app.listen(port, () => console.log(`api listening on port ${port}`))
